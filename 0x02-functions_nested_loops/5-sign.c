9f6ae18cebaee8c862364ccfa5fd0732084a097f
BenedictApp <95benedict@gmail.com>
2023-03-22 09:04:17 -0700

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -13,17 +13,17 @@ int print_sign(int n)
 		return (1);
 
 	}
-	else if (n == 0)
+	else if (n < 0)
 	{
 		_putchar(45);
-		return (0);
+		return (-1);
 
 	}
 	else
 	{
 
 		_putchar(48);
-		return (-1);
+		return (0);
 
 	}
 }
