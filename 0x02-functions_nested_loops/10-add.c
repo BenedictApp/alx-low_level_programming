c44b1ead24215d129a6586cec2c54a2bc80ce483
BenedictApp <95benedict@gmail.com>
2023-03-22 05:34:56 -0700

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+*add - add tow numbers
+*@firstNum: the first number to be computed
+*@secondNum: the second number to be computed
+*Return:Integer value
+ */
+int add(int firstNum, int secondNum)
+{
+	return (firstNum + secondNum);
+
+}
