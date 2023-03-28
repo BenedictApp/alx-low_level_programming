ce81123d9ace35c50ba67d2856e1032ec8455fad
BenedictApp <95benedict@gmail.com>
2023-03-28 04:11:12 -0700

    Half the lies they tell about me aren't true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+  *puts2 - print a string
+  *
+  *@str: value to print
+  *
+  *Return: nothing
+  */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
