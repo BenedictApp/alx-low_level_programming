5b3311fddefc2b3bc5a470540251068baa405cd8
BenedictApp <95benedict@gmail.com>
2023-03-29 04:46:24 -0700

    0. strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ *_strcat - function that concatenate src string to dest string
+ *
+ *@dest: Destination string
+ *
+ *@src: Source string
+ *
+ *Return: destination string
+ */
+char *_strcat(char *dest, char *src)
+{
+	int dest_length = 0;
+
+	int src_length = 0;
+
+	int i;
+
+	for (i = 0; dest[i] != '\0'; i++)/* finding the length of the dest string */
+		dest_length++;
+	for (i = 0; src[i] != '\0'; i++)/* finding the length of the src string */
+		src_length++;
+	for (i = 0; i <= src_length; i++)
+	{
+		dest[dest_length + 1] = src[i];/* cat. the src string to dest string */
+	}
+	return (dest);
+}
