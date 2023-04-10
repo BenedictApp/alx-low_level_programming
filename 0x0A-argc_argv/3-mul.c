51b92cc6fc26aa0119eb812fe2b326327b13fa42
BenedictApp <95benedict@gmail.com>
2023-04-10 03:12:01 -0700

    Neither irony nor sarcasm is argument

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+  *main - print the multiple of 2 integers
+  *
+  *@argc: arg count
+  *
+  *@argv: arg vector
+  *
+  *Return: always 0
+  */
+int main(int argc, char *argv[])
+{
+	int n1 = 0, n2 = 0;
+
+	if (argc == 3)
+	{
+		n1 = atoi(argv[1]);
+		n2 = atoi(argv[2]);
+		printf("%d\n", n1 * n2);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
